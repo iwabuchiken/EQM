41e6ca96d34fdc248551192badc38372a8354009
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-01-06 21:56:34 +0900

    m
    v-1.0
    ---
    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EQM::Application.config.secret_token = '1fc77209afccf0b68b2e1acb11e0c533205ea961c9c805c403d7512cbcadade47fabaf7670b6cea173033a1e298b4c33fffe0d3839c7f6987b91e198f04be8a8'
